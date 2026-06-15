f4138bb567d6f5f5ba17dc5cc8f2e49476983ff4
giantshyper <giants_hyper.6z@icloud.com>
2026-06-15 16:01:29 +0100

    Daniel Monteiro photo path to v4 (face crop)

--- players.js
+++ players.js
@@ -32,7 +32,7 @@ const teams = {
     { number: 41, name: "Martim Cardoso",     photo: "assets/sub11b/martim-cardoso.jpg" },
     { number:  6, name: "Lourenço Jesus",     photo: "assets/sub11b/lourenco-rebelo-de-jesus.jpg" },
     { number:  8, name: "João Ferreira",      photo: "assets/sub11b/joao-ferreira-v2.jpg" },
-    { number: 84, name: "Daniel Monteiro",    photo: "assets/sub11b/daniel-monteiro-v3.jpg" },
+    { number: 84, name: "Daniel Monteiro",    photo: "assets/sub11b/daniel-monteiro-v4.jpg" },
     { number:  9, name: "José Pereira",       photo: "assets/sub11b/jose-miguel-pereira.jpg" }
   ],
   "Sub-13": [
